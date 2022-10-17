420cdced14a2d6f726d71c07ee656a9a69ad0536
hong-sile <gurwns9325@naver.com>
2022-10-17 14:32:01 +0900

    style : 상수명 재정의

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -8,9 +8,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseballNumber {
 	private List<Integer> baseballNumber = new ArrayList<>();
-	private static final String zero= new String("0");
-	private static final String negative= new String("-");
-	private static final Integer baseBallNumberSize = 3;
+	private static final String ZERO = "0";
+	private static final String NEGATIVE = "-";
+	private static final Integer BASE_BALL_NUMBER_SIZE = 3;
 
 	public BaseballNumber() {}
 	private List<Integer> convertIntegerList(String input) {
@@ -18,15 +18,15 @@ public class BaseballNumber {
 	}
 
 	private void validateInput(String input) {
-		if (input.contains(zero)) throw new IllegalArgumentException("0은 유효한 값이 아닙니다.");
-		if (input.contains(negative)) throw new IllegalArgumentException("음수는 유효한 값이 아닙니다.");
-		if (input.length() != baseBallNumberSize) throw new IllegalArgumentException("입력값의 자릿수가 3이 아닙니다.");
+		if (input.contains(ZERO)) throw new IllegalArgumentException("0은 유효한 값이 아닙니다.");
+		if (input.contains(NEGATIVE)) throw new IllegalArgumentException("음수는 유효한 값이 아닙니다.");
+		if (input.length() != BASE_BALL_NUMBER_SIZE) throw new IllegalArgumentException("입력값의 자릿수가 3이 아닙니다.");
 		if (input.chars().distinct().count() != input.length())
 			throw new IllegalArgumentException("세 숫자 중 중복된 숫자가 존재합니다.");
 	}
 
 	public void initNumber() {
-		while (baseballNumber.size() != baseBallNumberSize) {
+		while (baseballNumber.size() != BASE_BALL_NUMBER_SIZE) {
 			int randomNumber = Randoms.pickNumberInRange(1, 9);
 			if (baseballNumber.contains(randomNumber)) continue;
 			baseballNumber.add(randomNumber);
