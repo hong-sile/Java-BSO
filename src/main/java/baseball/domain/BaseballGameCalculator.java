e55a96dee8125ab5c3e26545733cd0de6320943b
hong-sile <gurwns9325@naver.com>
2022-10-17 14:03:02 +0900

    refactor : 리스트를 합치는 메소드 리팩토링

--- src/main/java/baseball/domain/BaseballGameCalculator.java
+++ src/main/java/baseball/domain/BaseballGameCalculator.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -7,10 +8,9 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BaseballGameCalculator {
-
 	public boolean calculateBSO(BaseballNumber answer, BaseballNumber input){
-		List<Integer> answerNumber = answer.getNumberList();
-		List<Integer> inputNumber = input.getNumberList();
+		List<Integer> answerNumber = answer.getBaseballNumber();
+		List<Integer> inputNumber = input.getBaseballNumber();
 		List<Integer> duplicateNumbers = createDuplicateList(answerNumber,inputNumber);
 		int strikeCount = 0;
 		int ballCount = 0;
@@ -33,16 +33,22 @@ public class BaseballGameCalculator {
 	}
 
 	private List<Integer> createDuplicateList(List<Integer> listOne, List<Integer> listTwo){
-		List<Integer> numbers = Stream.concat(listOne.stream(),listTwo.stream())
-				.collect(Collectors.toList());
+		List<Integer> numbers = concatTwoList(listOne, listTwo);
 		Set<Integer> duplicateFilter = new HashSet<>();
 
 		return numbers.stream().filter(n -> !duplicateFilter.add(n))
 				.collect(Collectors.toList());
 	}
 
+	private static List<Integer> concatTwoList(List<Integer> listOne, List<Integer> listTwo) {
+		List<Integer> numbers = new ArrayList<>();
+		numbers.addAll(listOne);
+		numbers.addAll(listTwo);
+		return numbers;
+	}
+
 	private void printResultOfGame(int strikeCount, int ballCount){
-		if(ballCount ==0 && strikeCount == 0) {
+		if(ballCount == 0 && strikeCount == 0) {
 			System.out.println("낫싱");
 			return;
 		}
