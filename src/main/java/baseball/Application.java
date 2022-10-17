d07a1488e8aad6584daad4551e64d2f0662c2647
hong-sile <gurwns9325@naver.com>
2022-10-17 15:17:26 +0900

    style : 필요없는 내용 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 	public static void main(String[] args) {
 		//TODO: 숫자 야구 게임 구현
-		Controller controller = new Controller();
-		controller.initGame();
+		new Controller().run();
 	}
 }
